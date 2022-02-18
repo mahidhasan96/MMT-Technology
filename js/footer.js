4b4224e9853f67247d6f3a9ac2f98017d79f0854
mahidhasan96 <mahidhasan1122@gmail.com>
2022-02-19 01:07:10 +0600

    adding some sections

---
+++ js/footer.js
@@ -0,0 +1,65 @@
+const createFooter = () =>{
+    let footer = document.querySelector('.footer');
+    
+    footer.innerHTML = `
+    
+    <div class="footer-content">
+    <img src="img/light-logo.png" class="logo" alt="">
+     <div class="footer-ul-content">
+         <ul class="category">
+             <li class="category-title">men</li>
+             <li><a href="#" class="footer-link">t-shirts</a></li>
+             <li><a href="#" class="footer-link">sweatshires</a></li>
+             <li><a href="#" class="footer-link">shirts</a></li>
+             <li><a href="#" class="footer-link">jeand</a></li>
+             <li><a href="#" class="footer-link">trouser</a></li>
+             <li><a href="#" class="footer-link">shoes</a></li>
+             <li><a href="#" class="footer-link">casuals</a></li>
+             <li><a href="#" class="footer-link">formal</a></li>
+             <li><a href="#" class="footer-link">watch</a></li>
+
+         </ul>
+         <ul class="category">
+             <li class="category-title">women</li>
+             <li><a href="#" class="footer-link">t-shirts</a></li>
+             <li><a href="#" class="footer-link">sweatshires</a></li>
+             <li><a href="#" class="footer-link">shirts</a></li>
+             <li><a href="#" class="footer-link">jeand</a></li>
+             <li><a href="#" class="footer-link">trouser</a></li>
+             <li><a href="#" class="footer-link">shoes</a></li>
+             <li><a href="#" class="footer-link">casuals</a></li>
+             <li><a href="#" class="footer-link">formal</a></li>
+             <li><a href="#" class="footer-link">watch</a></li>
+
+         </ul>
+     </div>
+</div>
+<p class="footer-title"> about company</p>
+     <p class="info">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate eligendi deleniti mollitia nisi, aspernatur alias at modi laudantium placeat quos perferendis sapiente consectetur labore amet omnis, architecto eaque temporibus sint praesentium repellendus autem ex? Accusantium, totam quisquam ipsum cupiditate dolore ex sapiente reiciendis assumenda labore sequi explicabo architecto perspiciatis aspernatur quod excepturi? Odit facere rerum pariatur veniam reiciendis cum ipsam debitis sunt ex iste, autem dicta, dolore repudiandae cumque atque id similique dolorem saepe unde quam deserunt. Iure, consectetur beatae at incidunt ab, repellendus ipsam eos voluptatibus, tempore officiis porro! Ad voluptate molestias numquam necessitatibus officia laudantium voluptas doloribus doloremque.
+         
+     </p>
+     <br>
+     <br>
+     <div class="info">support email - mahidhasan1122@gmail.com</div>
+     <br>
+     <br>
+     <div class="info">phone: 01515674270</div>
+
+     <div class="footer-social-container">
+         <div>
+             <a href="#" class="social-link">terms & services</a>
+             <a href="" class="social-link">privacy page</a>
+
+         </div>
+         <div>
+             <a href="#" class="social-link">facebook</a>
+             <a href="" class="social-link">twitter</a>
+             <a href="" class="social-link">instgram</a>
+
+
+         </div>
+     </div>
+     <div class="footer-credit">Clothing, Best apparels online store</div>`
+}
+
+createFooter();
